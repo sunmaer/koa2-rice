a2dabd6a8f1a5bac3b5b05177c35e248537a2859
sunmaer <878131732@qq.com>
2018-05-28 21:31:25 +0800

    解决回调函数地狱

--- controllers/rice/riceController.js
+++ controllers/rice/riceController.js
@@ -14,47 +14,51 @@ class RiceController {
   static async recongnition(ctx) {
     let req = ctx.req
     let busboy = new Busboy({headers: req.headers})
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-      let filePath = path.resolve(__dirname, `../../public/${filename}`)
-      // 暂存临时文件
-      file.pipe(fs.createWriteStream(filePath))
-      file.on('end', () => {
-        // 根据颜色特征识别图片
-        getAverageColorVector().then((res) => {
-          gm(filePath)
-          .resize(200, 200, '!')
-          .write(filePath, (err) => {
-            if(err) {
-              console.log(err)
-            } else {
-              // OpenCV 读取图像，获取图像像素矩阵
-              cv.readImage(filePath, (err, img) => {
-                if(err) {
-                  console.log(chalk.red(err))
-                } else {
-                  let vector = feature.color(img), type = '', minDistance
-                  res.forEach((v, i) => {
-                    let sum = 0, distance
-                    vector.forEach((item, index) => {
-                      sum += (item - v.vector[index]) * (item - v.vector[index])
+    var p = new Promise((resolve, reject) => {
+      busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+        let filePath = path.resolve(__dirname, `../../public/${filename}`)
+        // 暂存临时文件
+        file.pipe(fs.createWriteStream(filePath))
+        file.on('end', () => {
+          // 根据颜色特征识别图片
+          getAverageColorVector().then((res) => {
+            gm(filePath)
+            .resize(200, 200, '!')
+            .write(filePath, (err) => {
+              if(err) {
+                console.log(err)
+              } else {
+                // OpenCV 读取图像，获取图像像素矩阵
+                cv.readImage(filePath, (err, img) => {
+                  if(err) {
+                    console.log(chalk.red(err))
+                  } else {
+                    let vector = feature.color(img), type = '', minDistance
+                    res.forEach((v, i) => {
+                      let sum = 0, distance
+                      vector.forEach((item, index) => {
+                        sum += (item - v.vector[index]) * (item - v.vector[index])
+                      })
+                      distance = Math.sqrt(sum)
+                      if(minDistance === void 0 || distance < minDistance) {
+                        minDistance = distance
+                        type = v.name
+                      }
                     })
-                    distance = Math.sqrt(sum)
-                    if(minDistance === void 0 || distance < minDistance) {
-                      minDistance = distance
-                      type = v.name
-                    }
-                  })
-                  ctx.body = responseObj(1, '识别成功', { type: type })
-                  // 删除临时文件
-                  fs.unlink(path.resolve(__dirname, `../../public/${filename}`), () => {})
-                }
-              })
-            }
+                    resolve(type)
+                    // 删除临时文件
+                    fs.unlink(path.resolve(__dirname, `../../public/${filename}`), () => {})
+                  }
+                })
+              }
+            })
           })
         })
       })
+      req.pipe(busboy)
     })
-    req.pipe(busboy)
+    let type = await p
+    ctx.body = ctx.body = responseObj(1, '识别成功', { type: type })
   }
 }
 
